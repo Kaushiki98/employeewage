ff95e42d8eb0de9de2419beaf8e586586ddd4ef4
kaushiki <kaushiki.ak88@gmail.com>
2021-01-06 22:30:56 +0530

    Employment payment added as daily wages

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 isPresent=1
+wagePerhour=20
+noOfHours=8
 checkAttendance=$((RANDOM%2))
 if [ $isPresent -eq $checkAttendance ]
 then
 	echo "Employee Present"
+	payment=$(($wagePerhour*$noOfHours))
+	echo "Payment is: "$payment
 else
 	echo "Employee Absent"
 	echo "Payment is 0"

e397283c5504c31dd9da64bbaaa5582fef34fe34
kaushiki <kaushiki.ak88@gmail.com>
2021-01-06 21:40:26 +0530

    employee attendance is added

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+echo "Welcome to Employee wage program"
 isPresent=1
+wagePerHour=20
+noOfHours=8
 checkAttendance=$((RANDOM%2))
 if [ $isPresent -eq $checkAttendance ]
 then
 	echo "Employee Present"
+	payment=$(($wagePerHour*$noOfHours))
+	echo "Payment is: "$payment
 else
 	echo "Employee Absent"
 	echo "Payment is 0"

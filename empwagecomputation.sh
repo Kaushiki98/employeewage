2bff7cb7ac78c0d56840cb8fab1523f26e8bdb86
kaushiki <kaushiki.ak88@gmail.com>
2021-01-06 22:53:35 +0530

    Employee part time salary is added

--- empwagecomputation.sh
+++ empwagecomputation.sh
@@ -1,18 +1,22 @@
-<<<<<<< HEAD
-echo "Welcome to the employee wage program"
-=======
 #!/bin/bash
+echo "Welcome to the employee wage program"
 isPresent=1
-wagePerhour=20
+isPartTime=2
+ratePerhour=20
 noOfHours=8
-checkAttendance=$((RANDOM%2))
+noOfPartTimeHours=4
+checkAttendance=$((RANDOM%3))
 if [ $isPresent -eq $checkAttendance ]
 then
 	echo "Employee Present"
-	payment=$(($wagePerhour*$noOfHours))
+	payment=$(($ratePerhour*$noOfHours))
 	echo "Payment is: "$payment
+elif [ $isPartTime -eq $checkAttendance ]
+then
+	echo "Employee is part time"
+	payment=$(($ratePerHour*$noOfPartTimeHours))
+	echo "Payments is : "$payment
 else
 	echo "Employee Absent"
 	echo "Payment is 0"
 fi
->>>>>>> EmployeeDailywageUC-2
